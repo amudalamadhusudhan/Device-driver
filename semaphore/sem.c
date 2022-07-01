54642220687ff12275bb153b3c210782b71fe93c
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-07-01 16:04:38 +0530

    sema

--- semaphore/sem.c
+++ semaphore/sem.c
@@ -9,17 +9,20 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
+#include <linux/semaphore.h>
 // Header files ends //////////////////////////////////////////////////////////////////
-
-dev_t dev = 0; // major number minor dynamic allocation //
+#define size 1024
+dev_t dev; // major number minor dynamic allocation //
 static struct cdev char_cdev;
+struct semaphore my_sema;
+unsigned int op_result;
 // function prototypes
 static void __exit driverE(void);
 static int __init driverI(void);
 static int char_open(struct inode *inode, struct file *file);
 static int char_release(struct inode *inode, struct file *file);
 static ssize_t char_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
-static ssize_t char_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+static ssize_t char_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .write = char_write,
@@ -30,23 +33,57 @@ static struct file_operations fops = {
 };
 static int char_open(struct inode *inode, struct file *file)
 {
-    pr_info("char driver open function called\n");
+    pr_info("sem driver open function called\n");
     return 0;
 }
 static int char_release(struct inode *inode, struct file *file)
 {
-    pr_info("char driver realease function called\n");
+    pr_info("sem driver realease function called\n");
     return 0;
 }
 static ssize_t char_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
 {
-    pr_info("char driver read function called\n");
+    printk("sem driver read function called\n");
+    int r;
+    r = copy_to_user(buf, &op_result, size);
+    if (r == 0)
+    {
+        printk("\n sucuss in reading data from thr kernal to usr\n");
+
+        printk("\n sucuss in reading data from thr kernal to usr %d\n", op_result);
+        up(&my_sema);
+        return size;
+    }
+    else
+    {
+        printk(" errot in reading data from  kernal to usr\n");
+        return -1;
+    }
+
     return 0;
 }
-static ssize_t char_write(struct file *filp, const char *buf, size_t len, loff_t *off)
+static ssize_t char_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
+    down(&my_sema);
+    printk("\nsem driver write function called\n");
+    int kbuff[2];
+    int r, n1, n2;
+    r = copy_from_user((char *)kbuff, (char *)buf, sizeof(kbuff));
+    if (r == 0)
+    {
+        printk("success in writing from the user to kernal\n");
+        n1 = kbuff[0];
+        n2 = kbuff[1];
+        op_result = n1 + n2;
+        printk("success in writing from the user to kernal reult =%d\n", op_result);
+        return len;
+    }
+    else
+    {
+        printk("error in writing data from user to kernal");
+        return -1;
+    }
 
-    pr_info("char driver write function called\n");
     return 0;
 }
 
@@ -54,10 +91,10 @@ static ssize_t char_write(struct file *filp, const char *buf, size_t len, loff_t
 
 static int __init driverI(void)
 {
-    alloc_chrdev_region(&dev, 0, 1, "simple char dynamic driver");
-    if ((alloc_chrdev_region(&dev, 0, 1, "simple char  dynamic driver")) < 0)
+    alloc_chrdev_region(&dev, 0, 3, "sem char dynamic driver");
+    if ((alloc_chrdev_region(&dev, 0, 3, "sem char  dynamic driver")) < 0)
     {
-        printk(KERN_ALERT "char driver inserted unsussfuly\n");
+        printk(KERN_ALERT "sem driver inserted unsussfuly\n");
         return -1;
     }
     printk(KERN_INFO "\n major =%d minor=%d\n", MAJOR(dev), MINOR(dev));
@@ -69,8 +106,8 @@ static int __init driverI(void)
     {
         pr_err("cannot add the device to the system");
     }
-
-    printk(KERN_ALERT "char device driver inserted successfuly\n");
+    sema_init(&my_sema, 1);
+    printk(KERN_ALERT "sema char device driver inserted successfuly\n");
     return 0; // return 0 for sucessfull compilation
 }
 
@@ -80,7 +117,7 @@ static void __exit driverE(void)
 {
     cdev_del(&char_cdev);
     unregister_chrdev_region(dev, 1);
-    printk(KERN_ALERT "\n char  dynamic device driver removed successfully\n");
+    printk(KERN_ALERT "\n sema char  dynamic device driver removed successfully\n");
 }
 
 module_init(driverI); // Module Initialization
